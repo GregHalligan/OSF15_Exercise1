41dd33566ac8fe068130acad7fc8ffc4cafe8da2
Greg Halligan <gahvg9@mail.missouri.edu>
2015-09-11 01:00:05 -0700

    Finished command.c

---
+++ command.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+#include "command.h"
+
+#define MAX_CMD_COUNT 50
+#define MAX_CMD_LEN 25
+
+
+	//TODO FUNCTION COMMENT
+
+/*
+   PURPOSE: This function parses the user input and figures out if they put in something valid. 
+   INPUT: it grabs the input from the user, and an array of commands 
+   RETURN: A bool either true or false.
+    
+*/
+bool parse_user_input (const char* input, Commands_t** cmd) {
+	
+	//TODO ERROR CHECK INCOMING PARAMETERS
+      
+
+//        if(input !=  cmd || cmd != input) 
+//        {
+//         return false;
+//        } 
+
+	char *string = strdup(input);
+	
+	*cmd = calloc (1,sizeof(Commands_t));
+	(*cmd)->cmds = calloc(MAX_CMD_COUNT,sizeof(char*));
+
+	unsigned int i = 0;
+	char *token;
+	token = strtok(string, " \n");
+	for (; token != NULL && i < MAX_CMD_COUNT; ++i) {
+		(*cmd)->cmds[i] = calloc(MAX_CMD_LEN,sizeof(char));
+		if (!(*cmd)->cmds[i]) {
+			perror("Allocation Error\n");
+			return false;
+		}	
+		strncpy((*cmd)->cmds[i],token, strlen(token) + 1);
+		(*cmd)->num_cmds++;
+		token = strtok(NULL, " \n");
+	}
+	free(string);
+	return true;
+}
+
+	//TODO FUNCTION COMMENT
+
+        /*
+         PURPOSE: Freeing the memory in the command array
+         INPUT: The command array
+         RETURN: VOID
+       */ 
+
+void destroy_commands(Commands_t** cmd) {
+
+	//TODO ERROR CHECK INCOMING PARAMETERS
+
+        if(cmd == NULL)
+        {
+           return;
+        } 
+
+	
+	for (int i = 0; i < (*cmd)->num_cmds; ++i) {
+		free((*cmd)->cmds[i]);
+	}
+	free((*cmd)->cmds);
+	free((*cmd));
+	*cmd = NULL;
+}
+
